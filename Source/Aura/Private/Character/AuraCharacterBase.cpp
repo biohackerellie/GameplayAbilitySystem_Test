53d3c66f050ca7e8e9028d4f605a8048bf49227a
Ellie <116388742+biohackerellie@users.noreply.github.com>
2024-01-14 14:26:04 -0700

    Derived classes

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,22 @@
+// Copyright EPKLabs
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright EPKLabs
+
+
+#include "Character/AuraEnemy.h"
+
